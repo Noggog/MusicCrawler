f6076b6c94ca9944dc2e81f0f3ced7b5afb9f2f1
Justin Swanson <justin.c.swanson@gmail.com>
2023-11-24 22:10:05 -0600

    Initial ChatGPT seeded code

---
+++ MusicCrawler.Lib/PlexApi.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace MusicCrawler.Lib;
+
+public class PlexApi
+{
+    private readonly HttpClient httpClient;
+    private readonly string baseUri;
+
+    public PlexApi(string baseUri, string token)
+    {
+        this.baseUri = baseUri;
+        this.httpClient = new HttpClient();
+        this.httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+        this.httpClient.DefaultRequestHeaders.Add("X-Plex-Token", token);
+    }
+
+    public async Task<Library[]> GetLibraries()
+    {
+        string url = $"{baseUri}/library/sections";
+        var response = await httpClient.GetStringAsync(url);
+        var data = JObject.Parse(response);
+        return data["MediaContainer"]["Directory"].ToObject<Library[]>();
+    }
+
+    public async Task<Item[]> GetRecentlyAdded(string libraryKey, int maxResults = 5)
+    {
+        string url = $"{baseUri}/library/sections/{libraryKey}/recentlyAdded?X-Plex-Container-Start=0&X-Plex-Container-Size={maxResults}";
+        var response = await httpClient.GetStringAsync(url);
+        var data = JObject.Parse(response);
+        return data["MediaContainer"]["Metadata"].ToObject<Item[]>();
+    }
+}
+
+public class Library
+{
+    public string Key { get; set; }
+    public string Title { get; set; }
+}
+
+public class Item
+{
+    public string Title { get; set; }
+    // Add other properties you want to retrieve
+}
---
+++ MusicCrawler/Program.cs
@@ -0,0 +1,22 @@
+﻿using MusicCrawler.Lib;
+
+PlexApi plex = new PlexApi(args[0], args[1]);
+
+try
+{
+    var libraries = await plex.GetLibraries();
+    foreach (var library in libraries)
+    {
+        Console.WriteLine($"Library: {library.Title} (Key: {library.Key})");
+
+        var recentlyAdded = await plex.GetRecentlyAdded(library.Key);
+        foreach (var item in recentlyAdded)
+        {
+            Console.WriteLine($"  - {item.Title}");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
