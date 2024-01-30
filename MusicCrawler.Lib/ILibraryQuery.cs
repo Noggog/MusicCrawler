482558fb67f18968b98d8f35f2bf6c149a1d2bfa
Justin Swanson <justin.c.swanson@gmail.com>
2024-01-29 20:16:31 -0600

    Initial umbrella interfaces, and some artist/album dtos
    closes #4
    closes #2

---
+++ MusicCrawler.Lib/ILibraryQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicCrawler.Lib;
+
+/// <summary>
+/// Interface for a source that can act as a user library, with a "finite" amount of artists/albums
+/// </summary>
+public interface ILibraryQuery : IMusicQuery
+{
+    Task<ArtistPackage[]> QueryAllData();
+}
