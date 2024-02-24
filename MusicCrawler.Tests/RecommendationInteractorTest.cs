6961e3c07c89ad40ad1f70b7880768148d09bac3
Troy <troyswanson1010@gmail.com>
2024-02-23 20:16:34 -0600

    created RecommendationInteractorTest

---
+++ MusicCrawler.Tests/RecommendationInteractorTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using MusicCrawler.Fakes.Services.Singletons;
+using MusicCrawler.Lib;
+using MusicCrawler.Lib.Services.Singletons;
+using Xunit;
+
+namespace MusicCrawler.Tests;
+
+public class RecommendationInteractorTest
+{
+    [Fact]
+    public async Task typical()
+    {
+        // # Given
+        var sut =
+            new RecommendationInteractor(
+                new FakeRecommendationRepo(),
+                new FakeLibraryQuery()
+            );
+        // # When
+        var result = await sut.Recommendations();
+        // # Then
+        result
+            .ToJson()
+            .Should().Be(
+                new Recommendation[]
+                {
+                    new Recommendation(
+                        Key: new ArtistKey(
+                            ArtistName: "fakeArtistName2"),
+                        SourceArtists: new[]
+                        {
+                            new ArtistKey(
+                                ArtistName: "fakeArtistName1")
+                        }
+                    )
+                }.ToJson());
+    }
+}
