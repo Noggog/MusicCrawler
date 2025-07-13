a54eff201680175c3edcea915922ba48a13b5de0
Justin Swanson <justin.c.swanson@gmail.com>
2025-07-12 20:14:07 -0500

    Added swagger

--- src/MusicCrawler.Backend/Program.cs
+++ src/MusicCrawler.Backend/Program.cs
@@ -1,14 +1,32 @@
 ﻿using MusicCrawler.Backend;
+using MusicCrawler.Lib;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddProblemDetails();
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Host.RegisterAutofacModule<MainModule>();
 
 var app = builder.Build();
 
 app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapDefaultEndpoints();
+
+app.MapGet("/artists", async (ILibraryQuery libraryQuery) =>
+    {
+        return await libraryQuery.QueryAllArtistMetadata();
+    })
+    .WithName("GetArtists");
 
 app.Run();
